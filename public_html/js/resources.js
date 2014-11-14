52fdc542694fdf62cc8b4d102b1d4c8f3dce0915
ricardoacevedo <funnyaf123@gmail.com>
2014-11-14 15:05:29 -0800

    more code  AND MAP

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -4,6 +4,8 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
+           {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+           {name: "mario", type:"image", src: "data/img/player1.png"},
 
 	/* Atlases 
 	 * @example
@@ -15,7 +17,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-
+          {name: "RicardoLevel01", type: "tmx", src: "data/map/Ricardolevel01.tmx"}
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
